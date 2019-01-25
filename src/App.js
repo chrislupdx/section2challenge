7b8ef326ff669a72f0ef70c3dede6f2422822cf8
cl <christopherlu.pdx@gmail.com>
2019-01-24 21:13:10 -0800

    added changehandler

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import React, { Component } from 'react';
 import Input from './Components/Input';
 class App extends Component {
+
+state = {
+  thingss : [
+    { id: "134134", output: "asdf" },
+    { id: "iobb", output: "aff"},
+    { id: "r90rwr", output: "ggdgdd"},
+    { id: "9oror", output: "ffdssfd"},
+  ],
+}
+
+
+
+  changeHandler = (event, id) => {
+    const Index = this.state.thingss.findIndex(t => {
+      return t.id === id;
+    });
+
+    const unit = {
+      ...this.state.thingss[Index]
+    };
+
+    unit.output = event.target.value;
+
+    const units = [...this.state.units];
+    units[Index] = unit;
+
+    this.setState( {units: units})
+
+  }
+
   render() {
     return (
       <div className="App">
--- src/Components/Input.js
+++ src/Components/Input.js
@@ -3,7 +3,7 @@ import React from 'react';
 const input = (props ) => {
   return (
     <div className ="Input">
-      <input type="text" />
+      <input type="text" onChange={props.changed} value={props.output} />
     </div>
   )
 };
