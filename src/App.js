691aba06fe4f7c92041e7a6d117740c98747f209
cl <christopherlu.pdx@gmail.com>
2019-01-27 16:19:00 -0800

    got long enough handler+state modifications done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Validationcomponent from './Components/Validation';
 class App extends Component {
   state = {
     inputtedwords: 'ugh', length: 0,
+    longenough: false,
+    tooshort: false,
   }
 
   lengthHandler = (event) => {
@@ -12,7 +14,33 @@ class App extends Component {
     console.log('ding');
   }
 
+  longenoughHandler = (event) => {
+    const longenough = this.state.longenough;
+    this.setState({longenough: !longenough});
+    //i'm pretty sure we need to inject conditional loggic here
+  }
+
   render() {
+
+    let longenough = null;
+    let tooshort = null;
+
+    if (this.state.length > 5) {
+      longenough = (
+        <div className="longenough">
+        <h1> Long enough </h1>
+        </div>
+      );
+
+    }
+    // else {
+    //   tooshort = (
+    //     <div className="tooshort">
+    //     <h1> Too short </h1>
+    //     </div>
+    //   );
+    // }
+
     return (
       <div className="App">
         <form>
@@ -21,11 +49,20 @@ class App extends Component {
           />
         </form>
 
+      <div className="validation">
         <Validationcomponent
-
           changed = {this.lengthHandler}
           length = {this.state.length}
         />
+        {longenough}
+        {tooshort}
+
+
+
+
+
+      </div>
+
       </div>
     );
   }
--- src/Components/Validation.js
+++ src/Components/Validation.js
@@ -5,8 +5,13 @@ const validationcomponent = ( props ) => {
     <div className = "validation">
     <input type = "text"
     onChange = {props.changed}
-    value={props.length} />
+    value={props.length}
+    />
     </div>
+
+
+      // <h1>"Too short"</h1>
+      // <h1>"Too long"</h1>
   )
 };
 
